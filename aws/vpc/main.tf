f180db1680c6c32140982808b4be881d096cc1e2
Alejandro Ochoa <alejo8a_2@hotmail.com>
2020-12-27 17:29:27 -0500

    Upgrade VPC to 0.13

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -297,7 +297,7 @@ resource "aws_security_group" "servers" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.ssh_cidr}"]
+    cidr_blocks = [var.ssh_cidr]
   }
 
   egress {
--- aws/vpc/outputs.tf
+++ aws/vpc/outputs.tf
@@ -1,27 +1,27 @@
 output "vpc_id" {
-  value = "${aws_vpc.main.id}"
+  value = aws_vpc.main.id
 }
 
 output "public_subnets_cidr" {
-  value = ["${aws_subnet.public.*.cidr_block}"]
+  value = [aws_subnet.public.*.cidr_block]
 }
 
 output "public_subnets" {
-  value = "${aws_subnet.public.*.id}"
+  value = aws_subnet.public.*.id
 }
 
 output "servers_sg_id" {
-  value = "${aws_security_group.servers.id}"
+  value = aws_security_group.servers.id
 }
 
 output "private_dbs_subnets_cidr" {
-  value = ["${aws_subnet.private_dbs.*.cidr_block}"]
+  value = [aws_subnet.private_dbs.*.cidr_block]
 }
 
 output "private_dbs_subnets" {
-  value = "${aws_subnet.private_dbs.*.id}"
+  value = aws_subnet.private_dbs.*.id
 }
 
 output "dbs_sg_id" {
-  value = "${aws_security_group.dbs.id}"
+  value = aws_security_group.dbs.id
 }
