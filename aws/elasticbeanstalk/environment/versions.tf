2660791a2bc07ab775e6c7116cec30557d93c50f
Alejandro Ochoa <alejo8a_2@hotmail.com>
2020-12-28 21:11:42 -0500

    Remove required providers

--- aws/elasticbeanstalk/environment/versions.tf
+++ aws/elasticbeanstalk/environment/versions.tf
@@ -1,10 +1,4 @@
 
 terraform {
-  required_providers {
-    aws = {
-      source  = "-/aws"
-      version = "~> 3.22"
-    }
-  }
   required_version = ">= 0.13"
 }
--- aws/rds/versions.tf
+++ aws/rds/versions.tf
@@ -1,9 +1,3 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "-/aws"
-      version = "~> 3.22"
-    }
-  }
   required_version = ">= 0.13"
 }
