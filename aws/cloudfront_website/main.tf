d5375099d7c40bb11a1d9bdd6080f5f10fe6f06c
PC\Aleochoam <alejo8a_2@>
2021-03-14 13:56:29 -0500

    Update bucket policy resource name

--- aws/cloudfront_website/main.tf
+++ aws/cloudfront_website/main.tf
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "s3_policy" {
   }
 }
 
-resource "aws_s3_bucket_policy" "example" {
+resource "aws_s3_bucket_policy" "policy" {
   bucket = module.bucket.bucket_id
   policy = data.aws_iam_policy_document.s3_policy.json
 }
